8118d0cb1d368bca443ed59d039d9b8d8ce39f86
최성재 <sungjaechoi39@gmail.com>
2024-03-26 02:28:01 +0900

    search section UI 구성

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -5,6 +5,9 @@ import Image from "next/image";
 import ZLogo from '../../../public/zlogo.png'
 import NavMenu from "@/app/(afterLogin)/_component/NavMenu";
 import LogoutButton from "./_component/LogoutButton";
+import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
+import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
+
 
 export default function AfterLoginLayout({ children }: { children: ReactNode }) {
   return (
@@ -41,6 +44,13 @@ export default function AfterLoginLayout({ children }: { children: ReactNode }) 
                 </svg>
                 <input type="search" />
               </form>
+            </div>
+            <TrendSection />
+            <div className={style.followRecommend}>
+              <h3>팔로우 추천</h3>
+              <FollowRecommend />
+              <FollowRecommend />
+              <FollowRecommend />
             </div>
           </section>
         </div>
