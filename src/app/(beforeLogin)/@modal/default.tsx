77325d0a9b6adc4cb0aa92679c3464b416771df2
최성재 <sungjaechoi39@gmail.com>
2024-03-25 14:59:27 +0900

    로그인전 ui

---
+++ src/app/(beforeLogin)/@modal/default.tsx
@@ -0,0 +1,8 @@
+export default function Default() {
+  return <div>응 안돼</div>;
+}
+
+// import Main from "@/app/(beforeLogin)/_component/Main";
+// export default function Default() {
+//   return <Main/>;
+// }
