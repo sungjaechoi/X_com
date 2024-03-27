5a6f0cfbe38092e8a37475ee90526e1f69d80461
최성재 <sungjaechoi39@gmail.com>
2024-03-27 11:50:20 +0900

    main home tab 구성

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,7 +1,27 @@
+import style from './home.module.css';
+import Tab from "./_component/Tab";
+import TabProvider from "./_component/TabProvider";
+import PostForm from "./_component/PostForm";
+import Post from "../_component/Post";
 export default function Home() {
   return (
-    <main>
-      홈페이지
+    <main className={style.main}>
+      <TabProvider>
+        <Tab/>
+        {/* <PostForm />
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/> */}
+      </TabProvider>
     </main>
   )
 }
