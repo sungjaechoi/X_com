a8035b86e4bd2dfac80da9a2979702bf0f96c41c
최성재 <sungjaechoi39@gmail.com>
2024-04-02 03:55:26 +0900

    explore UI 구성

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -2,8 +2,13 @@
 
 import style from '@/app/(afterLogin)/_component/trendSection.module.css';
 import Trend from './Trend';
+import { usePathname } from 'next/navigation';
+
 
 export default function TrendSection () {
+  // 현재 페이지 경로가 /explore 일때, 노출하지 않는다. - 나를 위한 트렌드는 탐색하기에 없음
+  const pathname = usePathname();
+  if (pathname === '/explore') return null;
   return(
     <div className={style.trendBg}>
       <div className={style.trend}>
