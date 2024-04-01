75e15a02c94607e120822b3e902e3dfc3b9b6866
최성재 <sungjaechoi39@gmail.com>
2024-04-02 02:45:10 +0900

    compose/tweet 게시하기 모달구성

--- src/app/(afterLogin)/layout.tsx
+++ src/app/(afterLogin)/layout.tsx
@@ -8,8 +8,8 @@ import LogoutButton from "./_component/LogoutButton";
 import TrendSection from "@/app/(afterLogin)/_component/TrendSection";
 import FollowRecommend from "@/app/(afterLogin)/_component/FollowRecommend";
 
-
-export default function AfterLoginLayout({ children }: { children: ReactNode }) {
+type Props = { children: ReactNode, modal : ReactNode }
+export default function AfterLoginLayout({ children, modal }: Props ) {
   return (
     <div className={style.container}>
       <header className={style.leftSectionWrapper}>
@@ -55,6 +55,7 @@ export default function AfterLoginLayout({ children }: { children: ReactNode }) 
           </section>
         </div>
       </div>
+      {modal}
     </div>
   )
 }
